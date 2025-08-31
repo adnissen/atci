658d22996b4d9fd5370c9fcb3891cc6c8772c7a7
Andrew Nissen <andrewnissen@Andrews-MacBook-Pro.local>
2025-08-31 15:50:02 -0400

    commit target stuff even though it should BE IGNORED

--- target/debug/deps/atci-8a01cc29f771424b.d
+++ target/debug/deps/atci-8a01cc29f771424b.d
@@ -1,6 +1,6 @@
-/Users/andrewnissen/atci/target/debug/deps/atci-8a01cc29f771424b.d: src/main.rs src/clipper.rs src/config.rs src/files.rs src/queue.rs src/video_processor.rs src/tools_manager.rs src/model_manager.rs src/search.rs src/transcripts.rs src/web.rs src/metadata.rs
+/Users/andrewnissen/atci/target/debug/deps/atci-8a01cc29f771424b.d: src/main.rs src/clipper.rs src/config.rs src/files.rs src/queue.rs src/video_processor.rs src/tools_manager.rs src/model_manager.rs src/search.rs src/transcripts.rs src/web.rs src/metadata.rs src/auth.rs
 
-/Users/andrewnissen/atci/target/debug/deps/atci-8a01cc29f771424b: src/main.rs src/clipper.rs src/config.rs src/files.rs src/queue.rs src/video_processor.rs src/tools_manager.rs src/model_manager.rs src/search.rs src/transcripts.rs src/web.rs src/metadata.rs
+/Users/andrewnissen/atci/target/debug/deps/atci-8a01cc29f771424b: src/main.rs src/clipper.rs src/config.rs src/files.rs src/queue.rs src/video_processor.rs src/tools_manager.rs src/model_manager.rs src/search.rs src/transcripts.rs src/web.rs src/metadata.rs src/auth.rs
 
 src/main.rs:
 src/clipper.rs:
@@ -14,3 +14,4 @@ src/search.rs:
 src/transcripts.rs:
 src/web.rs:
 src/metadata.rs:
+src/auth.rs:
